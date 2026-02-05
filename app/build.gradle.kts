c80fec7d4ed2a6143a9230d88c9b2bdcd2652633
ichaiwizm <ichaiwizlol@gmail.com>
2026-02-05 10:12:38 +0200

    chore: Bump version to 2.0.1 (versionCode 11)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ichaiwizm.metaraybanassistant"
         minSdk = 29  // Android 10 - Required by Meta SDK
         targetSdk = 34
-        versionCode = 10
-        versionName = "2.0.0"
+        versionCode = 11
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
